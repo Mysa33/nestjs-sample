009a712e8512226fea7ddd30a9c21f5e61acae84
Tlili <tpdrn.france@gmail.com>
2019-05-18 13:42:25 +0200

    Nest router module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { HttpModule} from '@nestjs/common';
+import { RouterModule } from 'nest-router';
+import {TypeOrmModule} from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'dexdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      keepConnectionAlive: true,
+      retryAttempts: 2,
+      retryDelay: 1000
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
