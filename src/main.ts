0c54d877f5b73b891faae63514805a002f6b4a0b
Tlili <tpdrn.france@gmail.com>
2019-05-31 13:41:17 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as cors from 'cors';
+import * as express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -8,15 +10,41 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-  .setTitle('@ Employees API')
-  .setDescription('The SG Employees API')
-  .setVersion('1.0')
-  .setBasePath('/')
-  .addTag('employees')
-  .build();
+  .new DocumentBuilder()
+    .setTitle('SG NestJS API demo')
+    .setDescription('SG NestJS API demo')
+    .setVersion('1.0')
+    .setContactEmail('example@sgcib.com')
+    .setTermsOfService('Internal usage')
+    .setHost('localhost:9999/api/v1')
+    .addOAuth2('implicit', oauth2Endpoint)
+    .build();
+  // All api endpoints will be prefixed with "/api/"
+  app.setGlobalPrefix('api/v1');
+  // Allow CORS requests
+  app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Cache-Control',
+      'Content-Type',
+      'If-Modified-Since',
+      'Origin',
+      'Pragma',
+      'X-Data-Count',
+      'X-Requested-With'
+    ],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }));
+  app.use(noCache);
   const document = SwaggerModule.createDocument(app, options);
   await SwaggerModule.setup('api', app, document);
   app.use('/static', express.static(__dirname + '/public'));
+  
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
