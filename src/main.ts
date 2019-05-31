65d90ddae3aca83fa3d391ee80330b22433dc407
Tlili <tpdrn.france@gmail.com>
2019-05-31 13:51:42 +0200

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,13 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-  .new DocumentBuilder()
     .setTitle('SG NestJS API demo')
     .setDescription('SG NestJS API demo')
     .setVersion('1.0')
     .setContactEmail('example@sgcib.com')
     .setTermsOfService('Internal usage')
     .setHost('localhost:9999/api/v1')
-    .addOAuth2('implicit', oauth2Endpoint)
+    //.addOAuth2('implicit', oauth2Endpoint)
     .build();
   // All api endpoints will be prefixed with "/api/"
   app.setGlobalPrefix('api/v1');
